a51de6a85aae6d212ba6f92a3f2d49aaea208c51
adebowale <ademibowaleadegboye@gmail.com>
2022-10-03 11:43:50 +0100

    modified class person

--- create_person.rb
+++ create_person.rb
@@ -28,38 +28,20 @@ class CreatePerson
           end
         
           def age_name
-    print 'Age:'
-    age = gets.chomp.to_i
-    print 'Name:'
-    name = gets.chomp
-
-    [age, name]
-  end
-
-  def collect_specialization
-    print 'Specialization:'
-    gets.chomp
-  end
-
-  def class_get
-    print 'Enter Classroom:'
-    gets.chomp
-  end
-
-  def create_student
-    age, name = age_name
-    classroom = class_get
-    Student.new(age, classroom, name)
-  end
-
-  def create_teacher
-    age, name = age_name
-    specialization = collect_specialization
-    Teacher.new(age, name, specialization)
-  end
-
-  def add_to_collection(person)
-    @people.push(person)
-    puts "#{person.name} added successfully"
-  end
-end
+            print 'Age:'
+            age = gets.chomp.to_i
+            print 'Name:'
+            name = gets.chomp
+        
+            [age, name]
+          end
+          def collect_specialization
+                    print 'Specialization:'
+                    gets.chomp
+                  end
+                
+                  def class_get
+                    print 'Enter Classroom:'
+                    gets.chomp
+                  end
+                
