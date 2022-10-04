4ffbedb68917f7ae0798fc7c263b0b8fef3cd6da
tahenyBELGUITH <taheny.belguith@gmail.com>
2022-10-04 09:33:32 +0100

    Modify book.rb

--- create_person.rb
+++ create_person.rb
@@ -26,3 +26,40 @@ class CreatePerson
     print 'Do you want to create a student (1) or a teacher (2)? [input a number]:'
     gets.chomp.to_i
   end
+
+   def age_name
+    print 'Age:'
+    age = gets.chomp.to_i
+    print 'Name:'
+    name = gets.chomp
+
+    [age, name]
+  end
+
+  def collect_specialization
+    print 'Specialization:'
+    gets.chomp
+  end
+
+  def class_get
+    print 'Enter Classroom:'
+    gets.chomp
+  end
+
+  def create_student
+    age, name = age_name
+    classroom = class_get
+    Student.new(age, classroom, name)
+  end
+
+  def create_teacher
+    age, name = age_name
+    specialization = collect_specialization
+    Teacher.new(age, name, specialization)
+  end
+
+  def add_to_collection(person)
+    @people.push(person)
+    puts "#{person.name} added successfully"
+  end
+end
