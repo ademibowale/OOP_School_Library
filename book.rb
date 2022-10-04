37613dfcdbcda1cfbf9882d7a3641a65479ce47c
adebowale <ademibowaleadegboye@gmail.com>
2022-10-04 16:03:04 +0100

    create open and close and io file

--- book.rb
+++ book.rb
@@ -1,13 +1,26 @@
 class Book
-  attr_accessor :title, :author, :rentals
-
-  def initialize(title, author)
-    @title = title
-    @author = author
-    @rentals = []
-  end
-
-  def add_rental(person, date)
-    Rental.new(date, self, person)
-  end
-end
+          attr_accessor :title, :author, :rentals
+        
+          def initialize(title, author)
+            @title = title
+            @author = author
+            @rentals = []
+          end
+        
+          def add_rental(person, date)
+            Rental.new(date, self, person)
+          end
+        
+          def to_json(*args)
+            {
+              JSON.create_id => self.class.name,
+              'arguments' => [@title, @author]
+            }.to_json(*args)
+          end
+        
+          # Deserialize JSON string by constructing new Foo object with arguments.
+          def self.json_create(object)
+            new(*object['arguments'])
+          end
+        end
+        
