3e2bd4420c234a18dc70d942a910bf29720bfb59
tahenyBELGUITH <taheny.belguith@gmail.com>
2022-10-05 16:55:04 +0100

    Use Chomp method

--- create_book.rb
+++ create_book.rb
@@ -10,24 +10,4 @@ class CreateBook
     new_book = Book.new(title, author)
     add_to_collection(new_book)
   end
-
-  private
-
-  def collect_book
-    print 'Book Title:'
-    title = gets.chomp
-    print 'Book Author:'
-    author = gets.chomp
-    [title, author]
-  end
-
-  def add_to_collection(new_book)
-    # book_new = JSON.generate(new_book)
-    @books.push(new_book)
-    puts "#{new_book.title} created successfully"
-    File.open('books.json', 'w+') do |file|
-      books = JSON.dump(@books)
-      file.write(books)
-    end
-  end
 end
